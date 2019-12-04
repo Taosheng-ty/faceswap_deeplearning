52ab342ec78efd23ce61b0cdd7e93f47b6e13d43
Taosheng-ty <taoyang@cs.utah.edu>
2019-12-04 16:04:53 -0700

    add optimizer and run function

--- utils/build_net.py
+++ utils/build_net.py
@@ -6,6 +6,72 @@ from torch.optim import lr_scheduler
 from torchvision import models
 import os
 from torchsummary import summary
+
+def get_optimizer(model):
+    """
+    Construct and return an Adam optimizer for the model with learning rate 1e-3,
+    beta1=0.5, and beta2=0.999.
+    
+    Input:
+    - model: A PyTorch model that we want to optimize.
+    
+    Returns:
+    - An Adam optimizer for the model with the desired hyperparameters.
+    """
+    optimizer = None
+    optimizer=optim.Adam(model.parameters(),lr=1e-3,betas=[0.5,0.999])
+    return optimizer
+def run_a_gan(D, G, D_solver, G_solver, discriminator_loss, generator_loss, show_every=250, 
+              batch_size=128, noise_size=96, num_epochs=10):
+    """
+    Train a GAN!
+    
+    Inputs:
+    - D, G: PyTorch models for the discriminator and generator
+    - D_solver, G_solver: torch.optim Optimizers to use for training the
+      discriminator and generator.
+    - discriminator_loss, generator_loss: Functions to use for computing the generator and
+      discriminator loss, respectively.
+    - show_every: Show samples after every show_every iterations.
+    - batch_size: Batch size to use for training.
+    - noise_size: Dimension of the noise to use as input to the generator.
+    - num_epochs: Number of epochs over the training dataset to use for training.
+    """
+    iter_count = 0
+    for epoch in range(num_epochs):
+        for x, _ in loader_train:
+            if len(x) != batch_size:
+                continue
+            D_solver.zero_grad()
+            real_data = x.type(dtype)
+#             print(real_data.size())
+            logits_real = D(2* (real_data - 0.5)).type(dtype)
+
+            g_fake_seed = sample_noise(batch_size, noise_size).type(dtype)
+            fake_images = G(g_fake_seed).detach()
+            logits_fake = D(fake_images.view(batch_size, 1, 28, 28))
+
+            d_total_error = discriminator_loss(logits_real, logits_fake)
+            d_total_error.backward()        
+            D_solver.step()
+
+            G_solver.zero_grad()
+            g_fake_seed = sample_noise(batch_size, noise_size).type(dtype)
+            fake_images = G(g_fake_seed)
+
+            gen_logits_fake = D(fake_images.view(batch_size, 1, 28, 28))
+            g_error = generator_loss(gen_logits_fake)
+            g_error.backward()
+            G_solver.step()
+
+            if (iter_count % show_every == 0):
+                print('Iter: {}, D: {:.4}, G:{:.4}'.format(iter_count,d_total_error.item(),g_error.item()))
+                imgs_numpy = fake_images.data.cpu().numpy()
+                show_images(imgs_numpy[0:16])
+                plt.show()
+                print()
+            iter_count += 1
+
 class Flatten(nn.Module):
     def forward(self, x):
         N, C, H, W = x.size() # read in N, C, H, W
