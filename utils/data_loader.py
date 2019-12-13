c89d3fafacf6b6d0979a88ecc947863395f5e05f
Taosheng-ty <taoyang@cs.utah.edu>
2019-12-12 22:37:55 -0700

    add a transfomration which can do to image and mask at the same time

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -38,7 +38,7 @@ def remove(x):
 class cycle_data_withfolder(Dataset):
     """Face Landmarks dataset."""
 
-    def __init__(self, img_dir,transform=None,dtype=torch.cuda.FloatTensor):
+    def __init__(self, img_dir,transform=None,data=None,dtype=torch.cuda.FloatTensor):
 #         print("init being called")
         self.img_dir=img_dir
         self.dirlist= os.listdir(img_dir)
@@ -57,9 +57,45 @@ class cycle_data_withfolder(Dataset):
         idx=idx%self.len
         img_dir=self.img_dir+self.dirlist[idx]
         self.img= PIL.Image.open(img_dir)
-        sample=self.transform(self.img)
+        if self.transform!=None:
+            img=self.transform(self.img)
+        sample={'rgb': img}
 #         sample=sample.type(self.dtype)
         return sample
+
+class cycle_data_withfolder_mask(Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, img_dir,mask_dir,transform=None,data=None,dtype=torch.cuda.FloatTensor):
+#         print("init being called")
+        self.img_dir=img_dir
+        self.data=data
+        self.mask_dir=mask_dir
+        self.dirlist= os.listdir(img_dir)
+        self.dirlist_mask= os.listdir(mask_dir)
+        self.len=len(self.dirlist)
+        self.transform = transform
+        self.dtype=dtype
+    def __len__(self):
+#         print("len called")
+        return 100000
+
+    def __getitem__(self, idx):
+#         print(idx,"idx")
+#         print("getitem idx",idx)
+#         print(self.img.size)
+        idx=idx%self.len
+        img_dir=self.img_dir+self.dirlist[idx]
+        mask_dir=self.mask_dir+self.dirlist_mask[idx]
+        self.img= PIL.Image.open(img_dir)
+        self.mask= PIL.Image.open(mask_dir)
+        img,mask=self.transform(self.img,self.mask,self.data)
+        mask=mask-torch.min(mask)
+        sample={'rgb': img ,'mask':mask}
+        
+#         sample=sample.type(self.dtype)
+        return sample
+
 if __name__ == "__main__":
     monet="/home/taoyang/research/research_everyday/faceswap-GAN/faceA/rgb/"
     data_transforms=transform()
