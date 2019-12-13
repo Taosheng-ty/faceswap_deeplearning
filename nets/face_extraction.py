456e727f970467c8320528654cf921398f3db153
Nicholas Ledwith <31265810+NLedwith@users.noreply.github.com>
2019-12-13 13:06:24 -0700

    Update face_extraction.py

--- nets/face_extraction.py
+++ nets/face_extraction.py
@@ -1,5 +1,53 @@
-def face_extraction(img):
-    ## extract faces and eyes from the img
-    ##especially gives the bounding box of the eyes and faces.
-    ##
-    pass
+import os
+import sys
+import cv2
+import imutils
+import numpy as np
+from PIL import Image
+
+
+# Takes an image, detects any faces in the image, and returns a list of the 
+# isolated face images.
+#
+# Parameters:
+#   scene- a single png image file
+#   model- the trained openCV facial recognition model
+# Returns:
+#   faces- list on images which is every isolated face from the scene
+def face_extraction(scene, model):
+  faces = []
+  updated_images = []
+
+  (h, w) = scene.shape[:2]
+  blob = cv2.dnn.blobFromImage(cv2.resize(scene, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+
+  model.setInput(blob)
+  detections = model.forward()
+
+  # Create frame around face
+  for i in range(0, detections.shape[2]):
+    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+    (startX, startY, endX, endY) = box.astype("int")
+
+    confidence = detections[0, 0, i, 2]
+
+    # If confidence > 0.5, show box around face
+    if (confidence > 0.5):
+      cv2.rectangle(scene, (startX, startY), (endX, endY), (255, 255, 255), 2)
+
+  updated_images.append(scene)
+
+  # Identify each face
+  count = 0
+  for i in range(0, detections.shape[2]):
+    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+    (startX, startY, endX, endY) = box.astype("int")
+
+    confidence = detections[0, 0, i, 2]
+    # If confidence > 0.5, save it as a separate file
+    if (confidence > 0.5):
+      count += 1
+      frame = scene[startY:endY, startX:endX]
+      faces.append(frame)
+
+  return faces
