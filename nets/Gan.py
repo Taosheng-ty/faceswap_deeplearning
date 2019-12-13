0a274501cb77630f616fe4047391b2940b4f3394
Taosheng-ty <taoyang@cs.utah.edu>
2019-12-12 22:36:50 -0700

    add mask loss  and

--- nets/Gan.py
+++ nets/Gan.py
@@ -26,10 +26,11 @@ from datetime import datetime
 import sys
 sys.path.append("../")
 import sys
+
 sys.path.append("/home/taoyang/research/Tao_lib/")
 from Tao_lib.log import Logger
-
 from utils import *
+from utils import my_segmentation_transforms
 from utils.loss import ls_generator_loss,ls_discriminator_loss
 from utils.loss import *
 dtype_float=torch.FloatTensor
@@ -40,39 +41,69 @@ dtype_float=torch.FloatTensor
 def cyclegan_ls_discriminator_loss(**data):
             real_A,real_B,D_A,D_B,G_A,G_B=data["real_A"],data[\
                  "real_B"],data["D_A"],data["D_B"],data["G_A"],data["G_B"]
+
+            
             reconstructed_A=G_A(real_A)
             reconstructed_B=G_B(real_B)
+            
+            
             reconstructed_A_score=D_A(reconstructed_A.detach())
             reconstructed_B_score=D_B(reconstructed_B.detach()) 
             real_A_score=D_A(real_A)
             real_B_score=D_B(real_B)
+            
+            
             loss_DA=ls_discriminator_loss(real_A_score,reconstructed_A_score)
             loss_DB=ls_discriminator_loss(real_B_score,reconstructed_B_score)
-            loss_D=(loss_DA+loss_DB)*2
+            loss_D=(loss_DA+loss_DB)*data["discriminator_loss_weight"]
             return loss_D
         
         
 def cyclegan_generator_loss(**data):
             real_A,real_B,D_A,D_B,G_A,G_B=data["real_A"],data[\
                  "real_B"],data["D_A"],data["D_B"],data["G_A"],data["G_B"]
+            real_A_mask=data["real_A_mask"]
+            real_B_mask=data["real_B_mask"]
             
             reconstructed_A=G_A(real_A)
             reconstructed_B=G_B(real_B)
+            
+            
             reconstructed_A_score=D_A(reconstructed_A)
             reconstructed_B_score=D_B(reconstructed_B)  
             cycle_A=G_A(G_B(real_A))
             cycle_B=G_B(G_A(real_A))
-            loss_G=ls_generator_loss(reconstructed_A_score)+ls_generator_loss(reconstructed_B_score)+(L1(reconstructed_B,real_B)+L1(reconstructed_A,real_A))*10+5*(L1(cycle_A,real_A)+L1(cycle_B,real_B))
+            loss_G=(ls_generator_loss(reconstructed_A_score)\
+            +ls_generator_loss(reconstructed_B_score))*data["fooling_loss_weights"]+(L1(reconstructed_B,real_B)+L1(reconstructed_A,real_A))*data["reconstruction_loss_weights"]+(L1(cycle_A,real_A)+L1(cycle_B,real_B))*data["cycle_loss_weights"]
+            
+            
+            
             if "edge" in data:
                 
                     loss_G+=data["edge"]*(edge_loss(reconstructed_A)+edge_loss(reconstructed_B)+\
                              edge_loss(cycle_A)+edge_loss(cycle_B))
+                    
+                    
             if "cnn" in data:
                 cnn=data["cnn"]
-                loss_G+=squzze_feature_loss(cnn,real_A,reconstructed_A)
-                loss_G+=squzze_feature_loss(cnn,real_B,reconstructed_B)
-                loss_G+=squzze_feature_loss(cnn,real_B,cycle_B)
-                loss_G+=squzze_feature_loss(cnn,real_A,cycle_A)
+                loss_G_pl=squzze_feature_loss(cnn,real_A,reconstructed_A)
+                loss_G_pl+=squzze_feature_loss(cnn,real_B,reconstructed_B)
+                loss_G_pl+=squzze_feature_loss(cnn,real_B,cycle_B)
+                loss_G_pl+=squzze_feature_loss(cnn,real_A,cycle_A)
+                loss_G=loss_G_pl*data["pl"]+loss_G
+            if "mask_A" in data:
+                real_B=real_B*real_B_mask
+                real_A=real_A*real_A_mask
+                reconstructed_A=reconstructed_A*real_A_mask
+                reconstructed_B=reconstructed_B*real_B_mask
+                reconstructed_A_score=D_A(reconstructed_A)
+                reconstructed_B_score=D_B(reconstructed_B)  
+                cycle_A=cycle_A*real_A_mask
+                cycle_B=cycle_B*real_B_mask
+                loss_G+=(L1(reconstructed_B,real_B)+L1(reconstructed_A,real_A))*data["reconstruction_loss_weights"]+\
+                (L1(cycle_A,real_A)+L1(cycle_B,real_B))*data["cycle_loss_weights"]
+
+               
             return loss_G
 
 
@@ -95,8 +126,10 @@ def run_a_cyclegan(G_A,D_A, G_B,D_B,G_solver, D_solver, discriminator_loss, gene
     """
     mean_running=0
     iter_count = 0
+    print(data)
+    dtype=data["dtype"]
     nn=0
-    save_iter=200
+    save_iter=1000
 #     loader_style_iter=iter(loader_content)       
     loader_content=data["A"]
     loader_style=data["B"]
@@ -115,17 +148,32 @@ def run_a_cyclegan(G_A,D_A, G_B,D_B,G_solver, D_solver, discriminator_loss, gene
     cnn=cnn.type(dtype)
     set_requires_grad(cnn,False)
     for epoch in range(num_epochs):
-        for i, (real_A, real_B,real_C) in enumerate(zip(loader_style, loader_content,other_Loader)):
+        for i, (real_A_dict, real_B_dict,real_C_dict) in enumerate(zip(loader_style, loader_content,other_Loader)):
             nn=nn+1
+#             print(real_A.keys(),real_A["rgb"].shape,real_A["mask"].shape)
+            real_A=real_A_dict["rgb"]
             real_A=real_A.type(dtype)
+        
+            real_A_mask=real_A_dict["mask"].type(dtype)
+            real_A_mask=real_A_mask.type(dtype)
+            
+            real_B=real_B_dict["rgb"]
             real_B=real_B.type(dtype)
+            
+            real_B_mask=real_B_dict["mask"]
+            real_B_mask=real_B_mask.type(dtype)
+            
+            real_C=real_C_dict["rgb"]
             real_C=real_C.type(dtype)
             if train_Discriminator_flag:
                 set_requires_grad(G_A,False)
                 set_requires_grad(G_B,False)
-                set_requires_grad([D_A,D_B],True)  
-                data={"real_A":real_A,"real_B":real_B,"D_A":D_A,"D_B":D_B,"G_A":G_A,"G_B":G_B}
-
+                set_requires_grad([D_A,D_B],True) 
+#                 print(data.keys(),"any key why none typebefoe???")
+                data.update({"real_A":real_A,"real_B":real_B,"real_A_mask":real_A_mask\
+                             ,"real_B_mask":real_B_mask,\
+                             "D_A":D_A,"D_B":D_B,"G_A":G_A,"G_B":G_B})
+#                 print(data.keys(),"any key why none type???")
                 loss_D=cyclegan_ls_discriminator_loss(**data)
                 D_solver.zero_grad()
                 loss_D.backward() 
@@ -135,8 +183,8 @@ def run_a_cyclegan(G_A,D_A, G_B,D_B,G_solver, D_solver, discriminator_loss, gene
             
             set_requires_grad([G_A,G_B],True)
             set_requires_grad([D_A,D_B],False) 
-            data={"real_A":real_A,"real_B":real_B,"D_A":D_A,"D_B":D_B,"G_A":G_A,"G_B":G_B}
-            data["edge"]=5e-4
+            data.update({"real_A":real_A,"real_B":real_B,"D_A":D_A,"D_B":D_B,"G_A":G_A,"G_B":G_B})
+           
             data["cnn"]=cnn
 
             loss_G=cyclegan_generator_loss(**data)
@@ -162,7 +210,7 @@ def run_a_cyclegan(G_A,D_A, G_B,D_B,G_solver, D_solver, discriminator_loss, gene
                 generated_A_from_C=G_A(real_C)
                 generated_B_from_C=G_B(real_C)    
                 cycle_A=G_A(G_B(real_A))
-                cycle_B=G_B(G_A(real_A))
+                cycle_B=G_B(G_A(real_B))
                 print(loss_D.cpu(),"loss of discriminator")
                 print(loss_G.cpu(),"loss of generator")
                 plt.figure()
@@ -335,10 +383,6 @@ def cycle_gan(**data):
     D_solver = torch.optim.Adam(itertools.chain(D_B.parameters(), D_A.parameters()),lr=1e-3,betas=[0.5,0.999])
     return G_A,D_A, G_B,D_B,  G_solver, D_solver
 
-
-
-
-
 if __name__ == "__main__":
 #     log_dir=Logger(data["ckpt_path"]+"/")
 #     sys.stdout=log_dir
@@ -361,6 +405,7 @@ if __name__ == "__main__":
     sys.stdout=Logger(log_dir+"/")
     G_A,D_A, G_B,D_B,  G_solver, D_solver=cycle_gan(**data)
     dtype = torch.cuda.FloatTensor
+    data["dtype"]=torch.cuda.FloatTensor
     plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
     plt.rcParams['image.interpolation'] = 'nearest'
     plt.rcParams['image.cmap'] = 'gray'
@@ -368,13 +413,13 @@ if __name__ == "__main__":
     data_transforms = {
     'train': T.Compose([
 #     T.RandomResizedCrop(256),
-       T.RandomRotation(10),
-    T.RandomHorizontalFlip(),
+#        T.RandomRotation(10),
+#     T.RandomHorizontalFlip(),
        T.Resize((data["size"],data["size"])),
-    T.RandomAffine(10,shear=10),
-    T.ColorJitter(0.2,0.2,0.2),
-    T.ToTensor(),
-    T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+#     T.RandomAffine(10,shear=10),
+#     T.ColorJitter(0.2,0.2,0.2),
+    T.ToTensor()
+#     T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
     'val': T.Compose([
     T.Resize(300),
@@ -385,15 +430,20 @@ if __name__ == "__main__":
 
     # monet="/raid/taoyang/research/research_everyday/repository/pytorch-CycleGAN-and-pix2pix/datasets/monet2photo/trainA/"
     # photo="/raid/taoyang/research/research_everyday/repository/pytorch-CycleGAN-and-pix2pix/datasets/monet2photo/trainB/"
-    A=data["A_dire"]
-    B=data["B_dire"]
-    C=data["C_dire"]
+    rgb_A=data["rgb_A"]
+    rgb_C=data["rgb_C"]
+    rgb_B=data["rgb_B"]
+    my_transforms=my_segmentation_transforms
     ImageFile.LOAD_TRUNCATED_IMAGES = True
-    A_=cycle_data_withfolder(A,data_transforms["train"],dtype)
-    B_=cycle_data_withfolder(B,data_transforms["train"],dtype)
-    C_=cycle_data_withfolder(C,data_transforms["train"],dtype)
-
-    loader_A = DataLoader(A_,
+    
+    mask_A=data["mask_A"]
+    mask_B=data["mask_B"]
+    A=cycle_data_withfolder_mask(rgb_A,mask_A,my_transforms,data)
+    B=cycle_data_withfolder_mask(rgb_B,mask_B,my_transforms,data)
+    C_=cycle_data_withfolder(rgb_C,data_transforms["train"])
+#     data={"size":64}
+#     single=cycle_data_withfolder_mask(A,B,my_segmentation_transforms,data)
+    loader_A = DataLoader(A,
                         batch_size=data["batch_size"],
                         num_workers=7,
                         shuffle=True)
@@ -401,7 +451,7 @@ if __name__ == "__main__":
     #                     batch_size=8,
     #                     num_workers=7,
     #                     shuffle=True)
-    loader_B= DataLoader(B_,
+    loader_B= DataLoader(B,
                         batch_size=data["batch_size"],
                         num_workers=7,
                         shuffle=True)
